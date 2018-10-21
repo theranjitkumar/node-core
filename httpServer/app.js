cca824cf1686d93c2d07d0794e3bab7f9b4dc321
theranjitkumar <web.ranjitkumar@gmail.com>
2018-10-21 15:58:12 +0530

    http server updated

--- httpServer/app.js
+++ httpServer/app.js
@@ -1,8 +1,13 @@
 const http = require('http');
+var fs = require('fs');
+
+var data = fs.readFileSync('index.html');
+
 http.createServer((req, res) => {
 	res.status = 200;
-	res.setHeader = ('content-type', 'text/html');
-	res.write('<h3>Hello world...</h3>');
+	res.setHeader = (200,'content-type', 'text/html');
+	// res.write('<h3>Hello world...</h3>');
+	res.write(data.toString());
 	res.end();
 }).listen(3000, () => {
 	console.log('server running on port 3000');
