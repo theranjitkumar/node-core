b4ebcd5d5c6990e0b203b52b5d7847f259691603
theranjitkumar <web.ranjitkumar@gmail.com>
2018-10-21 13:17:49 +0530

    routing added

--- httpServer_with_routing/app.js
+++ httpServer_with_routing/app.js
@@ -1,9 +1,17 @@
 const http = require('http');
 http.createServer((req, res) => {
-	res.status = 200;
-	res.setHeader = ('content-type', 'text/html');
-	res.write('<h3>Hello world...</h3>');
-	res.end();
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	var url = req.url;
+	 if(url ==='/about'){
+		res.write('<h1>about us page<h1>');
+		res.end();
+	 }else if(url ==='/contact'){
+		res.write('<h1>contact us page<h1>');
+		res.end();
+	 }else{
+		res.write('<h1>Hello World!<h1>');
+		res.end();
+	 }
 }).listen(3000, () => {
 	console.log('server running on port 3000');
 });
