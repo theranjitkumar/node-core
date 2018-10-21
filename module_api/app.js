fa6a7a3860df455ffe725067e97018a6e9672376
theranjitkumar <web.ranjitkumar@gmail.com>
2018-10-21 13:59:10 +0530

    module api updated

--- module_api/app.js
+++ module_api/app.js
@@ -4,7 +4,6 @@ const math  = require('./math');
 const m = new math(5);
 console.log(m.area());
 
-// const calc = new calculator();
 console.log(calculator.add(10,5));
 console.log(calculator.subtract(10,5));
 console.log(calculator.multiply(10,5));
